bf107050c6d58b3144777e5eddfd293b54650f4b
Rommy Elyan <relyan@email.arizona.edu>
2018-03-28 17:49:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
+
 function shout(string) {
   return string.toUpperCase()
 }
@@ -14,7 +28,7 @@ function sayHiToGrandma(string) {
   if (string==="I love you, Grandma.") {
   return "I love you, too."
 }
-} if (string===string.toUpperCase()){
+} if (string===uppercase){
   return 'YES INEED!'
 }
   if(string===lowercase){
