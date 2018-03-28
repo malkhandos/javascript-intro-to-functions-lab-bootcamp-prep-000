dc4a58677564bdfaa17a82407ba104c75d6b9236
Rommy Elyan <relyan@email.arizona.edu>
2018-03-28 17:46:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string==="I love you, Grandma.")
+  if (string==="I love you, Grandma.") {
   return "I love you, too."
+}
+}else if (string===string.toUpperCase()){
+  return 'YES INEED!'
 }
 function sayHiToGrandma(string) {
   if(string===lowercase){
