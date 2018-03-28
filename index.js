8e63939ebc7b40fcb889a372108cdd6af5aace53
Rommy Elyan <relyan@email.arizona.edu>
2018-03-28 17:41:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase
-
 function shout(string) {
   return string.toUpperCase()
 }
