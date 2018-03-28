c570a7cc1b41f72de3682dd0c6735d4df757d6ad
Rommy Elyan <relyan@email.arizona.edu>
2018-03-28 18:06:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'hello!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false
-
 function shout(string) {
   return string.toUpperCase()
 }
@@ -25,10 +11,15 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string===uppercase){
-  return 'YES INDEED!'}
-  else {
-  return 'I can\'t hear you!'}
+  if (string===string.toUpperCase()){
+  return 'YES INDEED!';
+  }
+  else if (string===string.toLowerCase()) {
+  return 'I can\'t hear you!'
+  }
+  else if (string==='I love you, Grandma.'){
+  return 'I love you, too.'
+  }
 
 }
 
