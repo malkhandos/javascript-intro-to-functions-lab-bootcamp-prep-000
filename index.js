e0633471a0523b87ce8272e219b89bdd074e74ce
Rommy Elyan <relyan@email.arizona.edu>
2018-03-28 16:56:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  var lowercase = "I can't hear you"
-  string.toLowerCase()===lowercase
+  var lowercase = "I can't hear you!"
+  lowercase.toLowerCase()===lowercase
   return "I can't hear you!"
 }
